a5772e3511c7a5239acc0f71a37c0adcb0bbf9ae
Mygel Bergstresser <FederationCoreY@gmail.com>
2015-06-24 17:25:54 -0400

    Added Footer
    - Added Footer
    - Adjust Font-Sizes For Mobile
    - Contact Page Adjusted for Mobile and Tablet

--- Content/js/main.js
+++ Content/js/main.js
@@ -13,7 +13,11 @@ $(document).ready(function () {
           $("#Navigation").slideToggle("fast");
       });
 
+    $('#top').on("click", function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000, function () {
 
+        });
+    });
 
 
 });
