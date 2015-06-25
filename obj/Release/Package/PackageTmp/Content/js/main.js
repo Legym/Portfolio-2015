e8c0da5c2198d6b8cee23721ef7624850903dd52
Mygel Bergstresser <FederationCoreY@gmail.com>
2015-06-25 02:29:25 -0400

    Mobile Portfolio
    - Portfolio Tab Redone for Mobile
    - Image compression
    - Workflow Step 3 Redesign Structure/Adjust Fonts/Color

--- obj/Release/Package/PackageTmp/Content/js/main.js
+++ obj/Release/Package/PackageTmp/Content/js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
-    $("#header").slideDown("slow");
 
-    $("#hamburger").click(function () {
-        $("#Navigation").slideToggle("fast");
+    $("#header").ready(function () {
+        $("#header").slideDown(1000, function () {
+            // Animation complete.
+        });
     });
+ 
+
+    document.querySelector("#nav-toggle")
+      .addEventListener("click", function () {
+          this.classList.toggle("active");
+          $("#Navigation").slideToggle("fast");
+      });
+
+    $('#top').on("click", function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000, function () {
+
+        });
+    });
+
 
 });
